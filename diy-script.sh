3973e360d3489594f0741eefd7d11ef6db82bfb2
zong <zong400@gmail.com>
2025-05-30 15:30:40 +0800

    add smartdns

--- diy-script.sh
+++ diy-script.sh
@@ -64,8 +64,8 @@ git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/l
 #sed -i "s|ARMv8|ARMv8_PLUS|g" package/luci-app-amlogic/root/etc/config/amlogic
 
 # SmartDNS
-#git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
-#git clone --depth=1 https://github.com/pymumu/openwrt-smartdns package/smartdns
+git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
+git clone --depth=1 https://github.com/pymumu/openwrt-smartdns package/smartdns
 
 # msd_lite
 #git clone --depth=1 https://github.com/ximiTech/luci-app-msd_lite package/luci-app-msd_lite
